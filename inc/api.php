9e46c80d18a0654d915795d8f291f0e3e868362c
Nilambar Sharma <nilambar@outlook.com>
2020-02-06 21:45:54 +0545

    Implement eng

--- inc/api.php
+++ inc/api.php
@@ -23,23 +23,49 @@ add_action( 'rest_api_init', function () {
 
 
 function ns_date_converter_api_callback( WP_REST_Request $request ) {
-	// print_r( $parameters = $request->get_params() );
-	$to = $request->get_param( 'to' );
+	$to   = $request->get_param( 'to' );
 	$date = $request->get_param( 'date' );
-	var_dump( $to );
-	var_dump( $date );
-	return;
+
 	// if ( ! in_array( $to, array( 'en', 'np' ), true ) ) {
 	// 	return new WP_Error( 'invalid_target', esc_html( 'Invalid target' ), array('status' => 404) );
 	// }
 
 	$output = array(
-		'date' => 'new',
+		'type' => esc_html( $to ),
 	);
 
-	$response = new WP_REST_Response($output);
+	$cal = new Nepali_Calendar();
 
-	$response->set_status(200);
+	list( $year, $month, $day ) = explode( '-', $date );
+
+	var_dump( $year );
+	var_dump( $month );
+	var_dump( $day );
+
+	switch ( $to ) {
+		case 'en':
+			if ( crossCheck( $year, $month, $day ) ) {
+				$new_date = $cal->nep_to_eng( $year, $month, $day );
+			}
+			$output['year']       = $new_date['year'];
+			$output['month']      = $new_date['month'];
+			$output['month_text'] = $new_date['emonth'];
+			$output['day']        = $new_date['date'];
+			$output['day_text']   = $new_date['day'];
+			$output['day_number'] = $new_date['num_day'];
+			break;
+
+		default:
+			# code...
+			break;
+	}
+
+				// print_r( $output );
+
+
+	$response = new WP_REST_Response( $output );
+
+	$response->set_status( 200 );
 
 	return $response;
 }
