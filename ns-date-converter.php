3693f44c83f499a2f39c8681db11e98ded8f0456
Nilambar Sharma <nilambar@outlook.com>
2022-11-07 15:04:35 +0545

    feat: update

--- ns-date-converter.php
+++ ns-date-converter.php
@@ -3,14 +3,11 @@
  * Plugin Name: NS Date Converter
  * Plugin URI: https://github.com/ernilambar/ns-date-converter/
  * Description: Provides shortcode for Nepali to English date converter. Shortcode: [ns_date_converter].
- * Version: 1.0.5
+ * Version: 1.0.6
  * Author: Nilambar Sharma
  * Author URI: https://www.nilambar.net/
  * License: GPL-2.0-or-later
  * Text Domain: ns-date-converter
- * GitHub Plugin URI: ernilambar/ns-date-converter
- * Primary Branch: main
- * Release Asset: true
  *
  * @package NS_Date_Converter
  */
@@ -22,15 +19,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'NS_DATE_CONVERTER_VERSION', '1.0.5' );
+define( 'NS_DATE_CONVERTER_VERSION', '1.0.6' );
+define( 'NS_DATE_CONVERTER_SLUG', 'ns-date-converter' );
 define( 'NS_DATE_CONVERTER_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NS_DATE_CONVERTER_URI', plugin_dir_url( __FILE__ ) );
 
 // Include autoload.
 if ( file_exists( NS_DATE_CONVERTER_DIR . '/vendor/autoload.php' ) ) {
 	require_once NS_DATE_CONVERTER_DIR . '/vendor/autoload.php';
+	require_once NS_DATE_CONVERTER_DIR . '/vendor/yahnis-elsts/plugin-update-checker/plugin-update-checker.php';
 }
 
 if ( class_exists( 'NSDateConverter\Init' ) ) {
 	\NSDateConverter\Init::register_services();
 }
+
+$nsdc_update_checker = \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker( 'https://github.com/ernilambar/ns-date-converter', __FILE__, NS_DATE_CONVERTER_SLUG );
+$nsdc_update_checker->getVcsApi()->enableReleaseAssets();
