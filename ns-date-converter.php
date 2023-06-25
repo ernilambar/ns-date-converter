ccf92d442c57500a235bc18fdee5619bb3dd035a
Nilambar Sharma <nilambar@outlook.com>
2023-06-25 10:57:29 +0545

    fix: conversion

--- ns-date-converter.php
+++ ns-date-converter.php
@@ -3,14 +3,14 @@
  * Plugin Name: NS Date Converter
  * Plugin URI: https://github.com/ernilambar/ns-date-converter/
  * Description: Provides shortcode for Nepali to English date converter. Shortcode: [ns_date_converter].
- * Version: 1.0.7
+ * Version: 1.0.8
  * Author: Nilambar Sharma
  * Author URI: https://www.nilambar.net/
  * License: GPL-2.0-or-later
  * Text Domain: ns-date-converter
  * Requires PHP: 5.6
  * Requires at least: 4.9
- * Tested up to: 6.1
+ * Tested up to: 6.2
  *
  * @package NS_Date_Converter
  */
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'NS_DATE_CONVERTER_VERSION', '1.0.7' );
+define( 'NS_DATE_CONVERTER_VERSION', '1.0.8' );
 define( 'NS_DATE_CONVERTER_SLUG', 'ns-date-converter' );
 define( 'NS_DATE_CONVERTER_DIR', plugin_dir_path( __FILE__ ) );
 define( 'NS_DATE_CONVERTER_URI', plugin_dir_url( __FILE__ ) );
