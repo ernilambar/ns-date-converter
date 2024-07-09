9045e5211dfba8db855ce029f6b7192edc99894d
Nilambar Sharma <nilambar@outlook.com>
2024-07-09 17:11:46 +0545

    Update packages

--- ns-date-converter.php
+++ ns-date-converter.php
@@ -9,9 +9,9 @@
  * License: GPL-2.0-or-later
  * Text Domain: ns-date-converter
  * Domain Path: /languages
- * Requires PHP: 5.6
- * Requires at least: 4.9
- * Tested up to: 6.2
+ * Requires PHP: 8.0
+ * Requires at least: 6.3
+ * Tested up to: 6.6
  *
  * @package NS_Date_Converter
  */
